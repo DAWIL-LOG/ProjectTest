0adc5afbca7f3623f0b6cff6ca8d81240f6598d9
DAWIL-Log <willnono8@gmail.com>
2022-08-01 03:06:54 +0100

    add onboarding

---
+++ app/src/main/java/com/dawil/testapp/OnBoarding.java
@@ -0,0 +1,34 @@
+package com.dawil.testapp;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OnBoarding extends Fragment {
+
+
+    public OnBoarding() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_on_boarding, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
