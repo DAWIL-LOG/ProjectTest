3dcb82e8196e630acaa77f5c9a38abd5e0d2b613
DAWIL-Log <willnono8@gmail.com>
2022-08-01 04:09:13 +0100

    Modify activity_main.xml to fragment_splash_screen.xml

---
+++ app/src/main/java/com/dawil/testapp/SplashScreenFragment.java
@@ -0,0 +1,46 @@
+package com.dawil.testapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SplashScreenFragment extends Fragment {
+
+    private NavController navController;
+    public SplashScreenFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        (new Handler()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navController.navigate(R.id.action_splashScreenFragment2_to_onBoarding);
+            }
+        }, 3000);
+    }
+}
